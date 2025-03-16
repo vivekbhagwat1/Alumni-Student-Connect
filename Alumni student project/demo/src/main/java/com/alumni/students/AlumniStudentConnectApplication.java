2e10b5722a0301cd701bc9485e65dc444d1404ed
vivekbhagwat1 <116779721+vivekbhagwat1@users.noreply.github.com>
2025-03-16 23:29:01 +0530

    Add files via upload

---
+++ Alumni student project/demo/src/main/java/com/alumni/students/AlumniStudentConnectApplication.java
@@ -0,0 +1,14 @@
+package com.alumni.students;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumniStudentConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumniStudentConnectApplication.class, args);
+
+	}
+
+}
---
+++ Alumni student project/demo/src/test/java/com/alumni/students/AlumniStudentConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alumni.students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumniStudentConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
